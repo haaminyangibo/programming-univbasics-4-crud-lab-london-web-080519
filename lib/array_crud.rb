9544d05ea98ea25e88f2f4caab61950e5857e6bf
Haami Nyangibo <haaminyangibo@gmail.com>
2019-08-01 11:34:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,8 +27,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  arr = ["am"]
-  arr[0]
+  arr = ["hello", "I","am"]
+  arr[2]
 end
 
 def retrieve_first_element_from_array(array)
