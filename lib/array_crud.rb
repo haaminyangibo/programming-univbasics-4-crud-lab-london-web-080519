97ab88b30cf41adf5fa660f0f78cf5b0fb89b568
Haami Nyangibo <haaminyangibo@gmail.com>
2019-08-01 11:36:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  arr ["wow", "hello", "haami"]
+  arr = ["wow", "hello", "haami"]
   arr[0]
 end
 
